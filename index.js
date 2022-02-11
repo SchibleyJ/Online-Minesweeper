e5074a67968c74f46f093783826a371fa60e7466
SchibleyJ <james@zoompuppy.com>
2022-02-11 10:53:20 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//websocket server 
+const WebSocket = require('ws');
+const server = require('http').createServer();
+const app = require('./httpServer.js');
+const port = process.env.PORT || 8080;
+
+const wss = new WebSocket.Server({ server: server, path: '/transfer' });
+
+server.on('request', app);
+
+
+server.listen(port, function () {
+    console.log(`http/ws server listening on ${port}`);
+});
