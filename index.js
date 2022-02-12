22ac28a8299b2b9b3aa6bfd7e5bae18ae37b4738
SchibleyJ <james@zoompuppy.com>
2022-02-11 22:32:36 -0500

    change name input to game room

--- index.js
+++ index.js
@@ -4,36 +4,48 @@ const server = require('http').createServer();
 const app = require('./httpServer.js');
 const port = process.env.PORT || 8080;
 
-const Board = require('./board.js')
+const Board = require('./board.js');
+const res = require('express/lib/response');
 
 const coopGames = {};
 
 const wss = new WebSocket.Server({ server: server, path: '/transfer' });
 
 server.on('request', app);
- 
+
 wss.on('connection', (client) => {
     client.on('message', (e) => {
         let message = JSON.parse(e);
         console.log(message)
-        switch (message.messageType){
+        switch (message.messageType) {
             case 0:
                 let id = Math.floor(Math.random() * 1000);
                 coopGames[id] = new Board(id, message.body.width, message.body.height, message.body.bombs);
-                client['userData'] = { gameId: id}
-                client.send(JSON.stringify({messageType: 0, body: { id: id }}));
+                //client['userData'] = { name: message.body.name, gameId: id }
+                client.send(JSON.stringify({ messageType: 0, body: { id: id } }));
                 break;
             case 1:
-                if (coopGames[message.body.id]){
-                    client['userData'] = { gameId: message.body.id}
-                    client.send(JSON.stringify({messageType: 0, body: { id: message.body.id }}));    
+                if (coopGames[message.body.id]) {
+                    client['userData'] = { name: message.body.name, gameId: message.body.id }
+                    client.send(JSON.stringify({
+                        messageType: 1, body: {
+                            bombs: coopGames[message.body.id].bombs,
+                            surroundings: coopGames[message.body.id].surroundings,
+                            clicks: coopGames[message.body.id].clicks,
+                            flags: coopGames[message.body.id].flags
+                        }
+                    }));
                 } else {
-                    client.send(JSON.stringify({messageType: 1, body: "Game not found." }));    
-                    
+                    // client.send(JSON.stringify({ messageType: 1, body: "Game not found." }));
+
                 }
                 break;
+
+
+
+
         }
-        
+
     })
 
 })
--- public/coop/coop.js
+++ public/coop/coop.js
@@ -1,16 +1,12 @@
 const code = (new URLSearchParams(window.location.search)).get('id');
 const ws = new WebSocket(`ws${location.protocol == "https:" ? 's' : ''}://${location.host}/transfer`);
 
-if (code === null) {
-    document.getElementById('start').classList.remove('hidden')
-} else {
-    document.getElementById('game').classList.remove('hidden')
-    ws.send(JSON.stringify({messageType: 2, body: {}}))
-}
+
 
 // CSS EDITIING
 let createModal = document.getElementById('create-modal');
 let joinModal = document.getElementById('join-modal');
+let nameModal = document.getElementById('name-modal');
 let startChoice = document.getElementById('start-choice')
 let closeCreate = document.getElementById("close-create");
 let closeJoin = document.getElementById("close-join");
@@ -44,17 +40,31 @@ window.onclick = function (event) {
 
 ws.onopen = () => {
     console.log('ws opened')
+
+    if (code === null) {
+        document.getElementById('start').classList.remove('hidden')
+    } else {
+        document.getElementById('game').classList.remove('hidden')
+        nameModal.style.display = 'block';
+        //ws.send(JSON.stringify({ messageType: 2, body: { id: code } }))
+    }
 }
 
 ws.onmessage = (e) => {
-    console.log(e)
+    console.log(e.data)
     const message = JSON.parse(e.data);
-    switch (message.messageType){
+    switch (message.messageType) {
         case 0:
             window.location.href = `${location.href}?id=${message.body.id}`;
             break;
         case 1:
             console.log(message.body);
+            break;
+        case 2:
+            linkModal.style.display = 'block';
+            break;
+        case 3:
+            console.log('here');
             break;
     }
 }
@@ -62,7 +72,7 @@ ws.onmessage = (e) => {
 const createGame = () => {
     let size = document.querySelector('input[name="size"]:checked').value;
     let width, height, bombs;
-    switch (parseInt(size)){
+    switch (parseInt(size)) {
         case 0:
             width = 9;
             height = 9;
@@ -81,16 +91,23 @@ const createGame = () => {
     }
     //let guesses = document.querySelector('input[name="guesses"]:checked').value;
     let guesses = true;
-    let name = document.getElementById('create-name').value;
+    //let name = document.getElementById('create-name').value;
     ws.send(JSON.stringify({ messageType: 0, body: { width: width, height: height, bombs: bombs, guesses: guesses, name: name } }))
 }
 
 const joinGame = () => {
-    let name = document.getElementById('join-name').value;
+    //let name = document.getElementById('join-name').value;
+    //let id = document.getElementById('join-code').value;
+    //ws.send(JSON.stringify({ messageType: 1, body: { name: name, id: id } }))
     let id = document.getElementById('join-code').value;
-    ws.send(JSON.stringify({ messageType: 1, body: {  name: name, id: id } }))
+    window.location.href = `${location.href}?id=${id}`;
 }
 
+const setName = () => {
+    let name = document.getElementById('name').value;
+    ws.send(JSON.stringify({ messageType: 1, body: { name: name, id: code } }))
+    nameModal.style.display = "none";
+}
 // GAME
 
 //setup board
@@ -101,8 +118,8 @@ const ctx = canv.getContext('2d');
 ctx.rect(100, 100, 600, 320);
 ctx.stroke();
 
-for (let i = 100; i < 700; i += 20){
-    for (let j = 100; j < 420; j+= 20){
+for (let i = 100; i < 700; i += 20) {
+    for (let j = 100; j < 420; j += 20) {
         drawSquare(i, j, 0);
     }
 }
@@ -112,6 +129,18 @@ function drawSquare(x, y, value) {
     let sprite = new Image;
     sprite.src = '/sprites/TileUnknown.png';
     sprite.onload = () => {
-        ctx.drawImage(sprite, x, y, 20, 20)    
+        ctx.drawImage(sprite, x, y, 20, 20)
     }
+}
+
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
 }
